481412eb560289745db9b36078a64b7b8396b725
saeedkefayati <s.kefayati2012@gmail.com>
2022-07-31 02:26:50 +0430

    update index

--- js/master.js
+++ js/master.js
@@ -1,13 +1,13 @@
 new Glide('.glide', {
-	type: 'slider',
-	startAt: 0,
+	type: 'carousel',
+	startAt: 1,
 	perView: 2,
 	focusAt: 'center',
 	gap: 24,
-	autoplay: 10000,
+	autoplay: 8000,
 	hoverpause: true,
 	keyboard: true,
-	rewind: false,
+	//rewind: false,
 	breakpoints: {
 		768: {
 			perView: 2,
